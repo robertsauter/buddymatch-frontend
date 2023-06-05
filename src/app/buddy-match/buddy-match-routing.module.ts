77f519329d5fb793fe3dd7b9c6268485c74bd8c9
sobertrauter <robert_sauter@gmx.de>
2023-06-05 13:45:32 +0200

    add static version of buddy match card

--- src/app/buddy-match/buddy-match-routing.module.ts
+++ src/app/buddy-match/buddy-match-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuddyMatchComponent } from './buddy-match.component';
 import { LoginComponent } from './login/login.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   { path: '', component: BuddyMatchComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'detail', component: DetailComponent }
 ];
 
 @NgModule({
