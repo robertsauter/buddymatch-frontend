7fa9eaa9b1177f6681728ee50bf2ed69ff7c9e84
sobertrauter <robert_sauter@gmx.de>
2023-06-08 12:08:01 +0200

    remove html boilerplate and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule) },
   { path: 'buddy-match', loadChildren: () => import('./buddy-match/buddy-match.module').then(m => m.BuddyMatchModule) }
 ];
