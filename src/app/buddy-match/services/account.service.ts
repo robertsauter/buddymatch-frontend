7a4b52efbb1cd4e2dff44df2d1daa8755ecf8ad7
sobertrauter <robert_sauter@gmx.de>
2023-06-25 17:30:53 +0200

    save token from backend

--- src/app/buddy-match/services/account.service.ts
+++ src/app/buddy-match/services/account.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import { UserDetail } from '../interfaces/user-detail';
 import { User } from '../interfaces/user';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'environment';
-import { BehaviorSubject, Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { ResponseData } from '../interfaces/responseData';
+import { Response } from '../interfaces/response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  email!: string;
+  user!: User;
+  token!: string;
 
   constructor(private http: HttpClient) {}
 
   checkIfUserIsLoggedIn(): boolean {
     const email = window.localStorage.getItem('email');
     if(email) {
-      this.email = email;
       return true;
     }
     return false;
   }
 
   login(email: string, password: string): Observable<ResponseData<null>> {
-    return this.http.post<HttpResponse<null>>(`${environment.baseUrl}/login`, { email, password }).pipe(
-      map(() => {
+    return this.http.post<Response>(`${environment.baseUrl}/login`, { email, password }).pipe(
+      map((response) => {
         window.localStorage.setItem('email', email);
-        this.email = email;
+        this.user = response.rows.user;
+        this.token = response.rows.token;
         return { isSuccess: true };
       }),
       catchError(() => {
