b04875d730a11745556ec363dcd6ea458522e4c5
Juan Sanchez <j.sanchezfedez@gmail.com>
2023-06-27 19:14:23 +0200

    Show users, and update information displayed for each user

--- src/app/buddy-match/buddy-match.component.ts
+++ src/app/buddy-match/buddy-match.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { AccountService } from './services/account.service';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-buddy-match',
@@ -7,4 +10,30 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class BuddyMatchComponent {
   @ViewChild('filtersModal') filtersModal!: HTMLDialogElement;
+
+  users: User[] = [];
+  currentUser: User | undefined;
+  currentIndex: number = 0;
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    //used dummy values in the params cause still don't know how to get the userId
+    this.userService.getUsers('userId', {filter1: 'value1', filter2: 'value2'}).subscribe(users => {
+      this.users = users;
+      if (users.length > 0) {
+        this.currentUser = users[0];
+      }
+    });
+  }
+
+  nextUser(): void {
+    this.currentIndex++;
+    if (this.currentIndex < this.users.length) {
+      this.currentUser = this.users[this.currentIndex];
+    } else {
+      //What to do when there is no more users
+    }
+  }
 }
+
--- src/app/buddy-match/services/user.service.ts
+++ src/app/buddy-match/services/user.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'; // Importamos HttpClient y HttpParams
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import { environment } from 'environment';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = `${environment.baseUrl}/:id/list`; // Base URL to get the list of users
 
-  constructor() { }
+  constructor(private http: HttpClient) { } // Inyect HttpClient
 
-  getUserByEmail(email: string) {}
+  getUserByEmail(email: string) {
+  }
 
   //Get all users, that match the given filter options. Should get the filters as input parameter
-  getUsers() {}
-}
+  getUsers(userId: string, filters: any): Observable<User[]> { 
+    let httpParams = new HttpParams();
+    Object.keys(filters).forEach(key => {
+      httpParams = httpParams.set(key, filters[key]);
+    });
+
+    return this.http.get<User[]>(`${this.baseUrl.replace(':id', userId)}`, { params: httpParams });
+  }
+
+
+  }
