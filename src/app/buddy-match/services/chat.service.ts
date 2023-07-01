711f962da534322cfa133340fe89bc0567111bf8
sobertrauter <robert_sauter@gmx.de>
2023-07-01 15:34:16 +0200

    add socket io and very basic chat

--- src/app/buddy-match/services/chat.service.ts
+++ src/app/buddy-match/services/chat.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(private socket: Socket) { }
 
-  sendMessage(message: string) {}
+  join(name: string) {
+    console.log('Try join:', name)
+    this.socket.emit('user_join', name);
+  }
+
+  sendMessage(message: string) {
+    this.socket.emit('chat_message', message);
+  }
+
+  receiveMessage(): Observable<{ user: string, message: string }> {
+    return this.socket.fromEvent('chat_message');
+  }
 
   //Get all messages for a single chat between two participants
   getMessages() {}
