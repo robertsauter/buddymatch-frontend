bd58df53aca2eed9768a0384bad23b805b27e7ea
sobertrauter <robert_sauter@gmx.de>
2023-07-03 10:47:40 +0200

    improve chat ux

--- src/app/buddy-match/pages/chat/chat.component.ts
+++ src/app/buddy-match/pages/chat/chat.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChatService } from '../../services/chat.service';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
-  partnerName: string = '';
+export class ChatComponent implements OnInit, AfterViewChecked {
+  chatPartner$!: Observable<User | null>;
+
+  @ViewChild('chatContainer') chatContainer!: ElementRef<HTMLDivElement>;
+
   userId: string = '';
   message: string = '';
+  messageReceived: boolean = true;
 
   messages: { sender: string, content: string }[] = [];
 
-  constructor(private route: ActivatedRoute, private chatService: ChatService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private chatService: ChatService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.userId = window.localStorage.getItem('userId') || '';
@@ -29,9 +40,23 @@ export class ChatComponent implements OnInit {
         sender: response.from,
         content: response.content
       });
+      this.messageReceived = true;
     });
 
-    this.chatService.connected$.subscribe((response) => this.messages = response.messages);
+    this.chatService.connected$.subscribe((response) => {
+      this.messages = response.messages;
+
+      const partnerId = response.participants.find((participant: string) => participant !== this.userId);
+      this.chatPartner$ = this.userService.getUserById(partnerId);
+      this.messageReceived = true;
+    });
+  }
+
+  ngAfterViewChecked(): void {
+    if(this.messageReceived) {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+      this.messageReceived = false;
+    }
   }
 
   sendMessage() {
