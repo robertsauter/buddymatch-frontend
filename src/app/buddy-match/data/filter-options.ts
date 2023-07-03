974707321a923dba11039e62efb89b681653918a
sobertrauter <robert_sauter@gmx.de>
2023-07-03 12:18:28 +0200

    add all filter options of dummy users

--- src/app/buddy-match/data/filter-options.ts
+++ src/app/buddy-match/data/filter-options.ts
@@ -1,14 +1,45 @@
 export const filterOptions = {
     studyPrograms: [
         'Komedia',
-        'Computer science'
+        'Informatik',
+        'Medienwissenschaften',
+        'Data Science',
+        'Mediendesign',
+        'Mobile App Development',
+        'Sozialwissenschaften'
     ],
     courses: [
-        'Advanced web technologies',
-        'Intelligent learning environments'
+        'Einführung in die Programmierung',
+        'Webentwicklung',
+        'Digitales Marketing',
+        'Kommunikationsstrategien',
+        'Statistik',
+        'Maschinelles Lernen',
+        'Grafikdesign',
+        'User Experience Design',
+        'Mobile Development',
+        'User Interface Design',
+        'Sozialpsychologie',
+        'Community-Management'
     ],
     skills: [
-        'JavaScript',
-        'Python'
+        'Java',
+        'HTML',
+        'CSS',
+        'Social Media Marketing',
+        'Content Creation',
+        'Marktforschung',
+        'Python',
+        'R',
+        'Datenanalyse',
+        'Adobe Photoshop',
+        'Adobe Illustrator',
+        'UI/UX Design',
+        'Swift',
+        'Kotlin',
+        'Mobile UI Design',
+        'Empathie',
+        'Projektmanagement',
+        'Öffentlichkeitsarbeit'
     ]
 };
