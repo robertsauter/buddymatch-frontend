9c0cda71cb189fb65f4498871be96d91c390c143
Juan Sanchez <j.sanchezfedez@gmail.com>
2023-07-09 12:07:14 +0200

    Tried to implement the fitlering

--- src/app/buddy-match/buddy-match.component.ts
+++ src/app/buddy-match/buddy-match.component.ts
@@ -1,7 +1,9 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { UserService } from './services/user.service';
-import { AccountService } from './services/account.service'; // Agregado
+import { AccountService } from './services/account.service'; 
 import { User } from './interfaces/user';
+import { FiltersModalComponent } from './pages/filters-modal/filters-modal.component';
+
 
 @Component({
   selector: 'app-buddy-match',
@@ -9,26 +11,29 @@ import { User } from './interfaces/user';
   styleUrls: ['./buddy-match.component.css']
 })
 export class BuddyMatchComponent {
-  @ViewChild('filtersModal') filtersModal!: HTMLDialogElement;
-
+  @ViewChild(FiltersModalComponent) filtersModal!: FiltersModalComponent;
+  
   users: User[] = [];
   currentUser: User | undefined;
   currentIndex: number = 0;
 
   constructor(private userService: UserService, private accountService: AccountService){}
+  
 
   ngOnInit(): void {
     let id: string = "";
 
     if (this.accountService.checkIfUserIsLoggedIn()) {
-      id = window.localStorage.getItem('userId') || "";  
+      id = window.localStorage.getItem('userId') || "";
     }
 
-    this.userService.getUsers(id, {filter1: 'value1', filter2: 'value2'}).subscribe(users => {
-      this.users = users;
-      if (users.length > 0) {
-        this.currentUser = users[0];
-      }
+    this.filtersModal.filtersSaved.subscribe(filters => {
+      this.userService.getUsers(id, filters).subscribe(users => {
+        this.users = users;
+        if (users.length > 0) {
+          this.currentUser = users[0];
+        }
+      });
     });
   }
 
