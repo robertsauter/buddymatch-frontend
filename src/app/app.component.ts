e6a06dd2ed1d70848f024ca8334c038379dd0969
sobertrauter <robert_sauter@gmx.de>
2023-08-01 22:17:34 +0200

    add loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, RouteConfigLoadStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  moduleLoading = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if(event instanceof RouteConfigLoadStart && event.route.path === 'buddy-match') {
+        this.moduleLoading = true;
+      }
+      else if(event instanceof NavigationEnd) {
+        this.moduleLoading = false;
+      }
+    });
+  }
+}
